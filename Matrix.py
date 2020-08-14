60c1638a2ee2c8148191376c83bfcb953cba9e7d
Rituraj Gupta <rituraj.iter@gmail.com>
2020-08-14 09:13:18 +0530

    Multiplication of matrices
    Change the matrices values and size according to the requirement

---
+++ Matrix.py
@@ -0,0 +1,9 @@
+x = [[4, 3], [2, 1]]
+y = [[4, 3], [2, 1]]
+result = [[0, 0], [0, 0]]
+for i in range(len(x)):
+    for j in range(len(y[0])):
+        for k in range(len(y)):
+            result[i][j] += x[i][k]*y[k][j]
+for r in result:
+    print(r)
